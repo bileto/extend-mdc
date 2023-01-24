ce2f63af8e13f705a63c87082d73ce64224687a9
Josef Petrak <josef@petrak.xyz>
2023-01-24 19:58:35 +0100

    Fixed example test

--- spring-boot-example/src/test/java/io/github/georgwittberger/extendmdc/example/ExampleTest.java
+++ spring-boot-example/src/test/java/io/github/georgwittberger/extendmdc/example/ExampleTest.java
@@ -1,35 +1,50 @@
 package io.github.georgwittberger.extendmdc.example;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import io.github.georgwittberger.extendmdc.example.service.ExampleService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.system.CapturedOutput;
-import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
-@ExtendWith(OutputCaptureExtension.class)
 @Import(TestAspectConfiguration.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ExampleTest {
   @Autowired
   public TestRestTemplate testRestTemplate;
 
-  @Test
-  public void testLogMessageContainsMDCValues(CapturedOutput output) {
-    //var restTemplate = applicationContext.getBean(TestRestTemplate.class);
+  private ListAppender<ILoggingEvent> logWatcher;
 
+  @BeforeEach
+  public void setupTest() {
+    logWatcher = new ListAppender<>();
+    logWatcher.start();
+    ((Logger) LoggerFactory.getLogger(ExampleService.class)).addAppender(logWatcher);
+  }
+
+  @Test
+  public void testLogMessageContainsMDCValues() {
     testRestTemplate.getForObject("/Johnny", String.class);
-    assertNotNull(output.toString());
-    assertEquals("UserName=Johnny", output.toString());
-    assertTrue(output.getOut().contains("UserName=Johnny"));
+
+    assertTrue(
+        logWatcher.list.stream()
+            .anyMatch(event -> {
+              var mdc = event.getMDCPropertyMap();
+
+              return mdc.containsKey("UserName") && mdc.get("UserName") != null && mdc.get("UserName").equals("Johnny");
+            }),
+        "MDC does not contain UserName=Johnny"
+    );
   }
 }
